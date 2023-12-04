57786f161d21b93ccf1087f1ac179945651494f6
DevineDesmondSA <71454349+DevineDesmondSA@users.noreply.github.com>
2023-12-04 13:05:16 -0800

    Update bot_bigergumba.py

--- bot_bigergumba.py
+++ bot_bigergumba.py
@@ -1,7 +1,12 @@
 def choice(round_score, my_score, opponent_score):
-    if (my_score >= 71) or (opponent_score >= 71):
-        return round_score >= (100 - my_score)
+    if ((round_score + my_score) >= 100):
+        return False
     else:
-        return round_score >= (21 + (abs(my_score - opponent_score)/8)
-
+        if (my_score >= 71) or (opponent_score >= 71):
+            return True
+        else:
+            if (round_score < (21 + (abs(my_score - opponent_score)/8))):
+                return True
+            else:
+                return False
 # 0.9% Disadvantage against Optimal Play. Read the Wikipedia article on Pig for more info.
